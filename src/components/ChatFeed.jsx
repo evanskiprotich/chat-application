6360c8ccdeb373e3f404c808a9bf7ca197775203
evanskiprotich <kiprotich.evans.kpngetich@gmail.com>
2021-10-14 14:02:26 +0300

    making changes on components

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,6 +1,29 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
 const ChatFeed = (props) => {
-    console.log(props);
+    const {chats, ActiveChat, userName, messages } = props;
 
+    const chat = chats && chats[ActiveChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        return keys.map(key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index ===0 ? null : keys[index - 1]
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key = {`msg_${index}`} style={{width:'100%'}}>
+<div className='message-block'>
+{
+    isMyMessage 
+    ? <MyMessage />
+}
+</div>
+                </div>
+            )
+        }
+    }
     return (
         <div>
             ChatFeed
